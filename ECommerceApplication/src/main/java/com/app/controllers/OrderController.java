a586c77f2dac051acae03f88adabe339ce9fcb5a
Muhammad Raditya Hanif <muhammad.raditya11@ui.ac.id>
2025-02-13 23:34:19 +0700

    feat: modify payment method to only accept COD

--- ECommerceApplication/src/main/java/com/app/controllers/OrderController.java
+++ ECommerceApplication/src/main/java/com/app/controllers/OrderController.java
@@ -2,16 +2,12 @@ package com.app.controllers;
 
 import java.util.List;
 
+import com.app.payloads.CreateAddressDTO;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.app.config.AppConstants;
 import com.app.payloads.OrderDTO;
@@ -27,12 +23,17 @@ public class OrderController {
 	
 	@Autowired
 	public OrderService orderService;
-	
+
 	@PostMapping("/public/users/{email}/carts/{cartId}/payments/{paymentMethod}/order")
-	public ResponseEntity<OrderDTO> orderProducts(@PathVariable String email, @PathVariable Long cartId, @PathVariable String paymentMethod) {
-		OrderDTO order = orderService.placeOrder(email, cartId, paymentMethod);
-		
-		return new ResponseEntity<OrderDTO>(order, HttpStatus.CREATED);
+	public ResponseEntity<OrderDTO> orderProducts(@PathVariable String email, @PathVariable Long cartId,
+												  @PathVariable String paymentMethod, @Valid @RequestBody CreateAddressDTO addressDTO) {
+		if (!paymentMethod.equals("cod")) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+		OrderDTO order = orderService.placeOrder(email, cartId, paymentMethod, addressDTO);
+
+		return new ResponseEntity<>(order, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/admin/orders")
